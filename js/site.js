2f36d0cd1c25e4bc4a501dd3b745302cb47f9bc5
Pat Lillis <lillispm@gmail.com>
2016-08-21 17:46:16 -0600

    add synths

---
+++ js/site.js
@@ -0,0 +1,34 @@
+var notes = ['C4', 'G4', 'D5'];
+var loops = [];
+var loopSynths = [];
+var singleSynths = [];
+
+for (var i = 0; i < 3; i++) {
+    var loopSynth = new Tone.Synth({ 'envelope': { 'release': 2 }}).toMaster();
+    loopSynths.push(loopSynth);
+
+    var singleSynth = new Tone.Synth({ 'envelope': { 'release': 2 }}).toMaster();
+    singleSynths.push(singleSynth);
+
+    loops.push(new Tone.Loop(play.bind(null, i, true), 1));
+}
+
+Tone.Transport.start();
+
+function play(number, loop) {
+    if (loop)
+        loopSynths[number].triggerAttackRelease(notes[number], 0.2);
+    else
+        singleSynths[number].triggerAttackRelease(notes[number], 0.2);
+}
+
+function repeatPlay(number, btn) {
+    if (btn.classList.contains('playing')) {
+        loops[number].stop();
+        btn.classList.remove('playing');
+    }
+    else {
+        loops[number].start();
+        btn.classList.add('playing');
+    }
+}
